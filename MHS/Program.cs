4167e0e134d2c3f0350ef3367fc86645db2d8dc2
BrofessorTec <125778428+BrofessorTec@users.noreply.github.com>
2024-03-24 20:13:38 -0400

    Reversed the isRunning values so that it makes more logical sense when to stop the loop. The Environment.Exit(0) should already exit out of the program when we want to quit too, but it doesnt hurt to have both as far as i know

--- MHS/Program.cs
+++ MHS/Program.cs
@@ -6,12 +6,12 @@
         {
             MemoryHierarchySimulator mhs = new MemoryHierarchySimulator();
 
-            bool isRunning = false;
+            bool isRunning = true;
             //This should likely be put in as one of the options able to be changed with 0. Edit Configuration alongside the capacityOfCache.
             string pathOfTrace = "trunc_12.dat";
 
 
-            while (!isRunning)
+            while (isRunning)
             {
 
                 mhs.ParseMemoryAddress(mhs.ReadMemoryTrace(pathOfTrace));
@@ -31,7 +31,7 @@
                     case "q":
                         Console.WriteLine("\nThanks for using the program, exiting now..");
                         Thread.Sleep(1000);
-                        isRunning = true;
+                        isRunning = false;
                         Environment.Exit(0);
                         break;
                     case "0":
